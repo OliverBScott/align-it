9c242da27f46829cfbe3fd0c463c5129b7579f75
OliverBScott <oliver.scott.17@ucl.ac.uk>
2021-12-17 19:23:16 +0000

    add unittests

---
+++ pyalignit/test.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2021 OliverBScott
+"""
+pyalignit.test
+--------------
+
+"""
+from rdkit import Chem
+import pyalignit
+
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    def test_AlignMol(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
