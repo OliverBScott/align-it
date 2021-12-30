e93adf05abbb538775a503f63ac71fc42cf845af
Oliver <33868948+OliverBScott@users.noreply.github.com>
2021-12-30 13:25:58 +0000

    Update __init__.py
    add docstrings

--- pyalignit/__init__.py
+++ pyalignit/__init__.py
@@ -8,19 +8,31 @@ Python wrappers for the Align-it™ tool from Silicos-it.
 import warnings
 
 from .cpyalignit import *
+
 from .draw import PHARM_COLORS, draw_pharmacophore
 
+# Get version information from cpp
 __version__ = GetVersion()
 
 
 class PharmacophoreSupplier(object):
-
+    """A class which supplies pharmacophores from a pharmacophore file."""
+    
     def __init__(self, filename):
+        """Initialize supplier with a filepath.
+        
+        Parameters
+        ----------
+        filename : str
+            A filepath pointing to a pharmacophore file (.phar)
+
+        """
         self._handle = open(filename, 'r')
         self._done = False
 
     @property
     def at_end(self):
+        """Returns whether or not we have hit EOF."""
         return self._done
 
     def _get_next_block(self):
@@ -82,20 +94,46 @@ class PharmacophoreSupplier(object):
 
 
 class PharmacophoreWriter(object):
+    """A class for writing pharmacophores to a pharmacophore file."""
 
     def __init__(self, filename):
+        """Initialize writer with a filepath.
+        
+        Parameters
+        ----------
+        filename : str
+            A filepath pointing to a pharmacophore file (.phar)
+
+        """
         self._handle = open(filename, 'w')
         self._num = 0
 
     @property
     def num_pharmacophores(self):
+        """Returns the number of pharmacophores written so far."""
         return self._num
 
     @property
     def closed(self):
+        """Returns whether the underlying file handle is closed."""
         return self._handle.closed
 
     def get_text(self, pharmacophore, name=None):
+        """Returns the PHAR text for a pharmacophore.
+        
+        Parameters
+        ----------
+        pharmacophore : Pharmacophore
+            The pharmacophore to write as text
+        name : str, optional
+            Optionally associate a name with the pharmacophore
+        
+        Returns
+        -------
+        str
+            string of pharmacophore in PHAR (.phar) format
+            
+        """
         name = name if name else ''
         text = f'NAME\t{name}\n'
         for point in pharmacophore:
@@ -103,15 +141,17 @@ class PharmacophoreWriter(object):
         text += '$$$$\n'
         return text
 
-    @staticmethod
-    def _point_to_text(p):
-        text = p.func.name + '\t'
-        text += f"{p.point.x:.6f}\t{p.point.y:.6f}\t{p.point.z:.6f}\t"
-        text += f"{p.alpha}\t{int(p.hasNormal)}\t"
-        text += f"{p.normal.x:.6f}\t{p.normal.y:.6f}\t{p.normal.z:.6f}\n"
-        return text
-
     def write(self, pharmacophore, name=None):
+        """Writes a pharmacophore to the output file.
+        
+        Parameters
+        ----------
+        pharmacophore : Pharmacophore
+            The pharmacophore to write to the file
+        name : str, optional
+            Optionally associate a name with the pharmacophore
+
+        """
         if self._handle.closed:
             raise IOError('Cannot write to a closed file')
         self._handle.write(self.get_text(pharmacophore, name))
@@ -119,7 +159,17 @@ class PharmacophoreWriter(object):
         self._num += 1
 
     def flush(self):
+        """Flushes the output file."""
         self._handle.flush()
 
     def close(self):
+        """Closes the output file."""
         self._handle.close()
+    
+    @staticmethod
+    def _point_to_text(p):
+        text = p.func.name + '\t'
+        text += f"{p.point.x:.6f}\t{p.point.y:.6f}\t{p.point.z:.6f}\t"
+        text += f"{p.alpha}\t{int(p.hasNormal)}\t"
+        text += f"{p.normal.x:.6f}\t{p.normal.y:.6f}\t{p.normal.z:.6f}\n"
+        return text
